8acf19af0c460246fb52d976033d87c71c30b5cf
Johnny Royall <johnnyroyall76@gmail.com>
2026-04-24 12:52:55 +0100

    Fix formatting of CORS initialization in app.py

--- app.py
+++ app.py
@@ -2,10 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})  
-
-
-app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 latest_image = None
 chosen_card = None
