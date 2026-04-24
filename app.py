c1a370a8a40fe60e397223ee9a4ed0d8b4a9a51f
Johnny Royall <johnnyroyall76@gmail.com>
2026-04-24 12:47:01 +0100

    Modify CORS setup in app.py
    Updated CORS configuration to allow all origins.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS   
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)   
+CORS(app, resources={r"/*": {"origins": "*"}})  
+
 
 app = Flask(__name__)
 
